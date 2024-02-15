3fac9a69fc79a7954ed2570bd68ccc90f649f14f
udana <udanaoneplus@gmail.com>
2024-02-15 16:54:22 +0530

    Add previous chat messages to new user

--- backend-ChatApp/src/main/java/lk/ijse/dep11/wecontroller/ChatWSController.java
+++ backend-ChatApp/src/main/java/lk/ijse/dep11/wecontroller/ChatWSController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lk.ijse.dep11.to.MessageTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -13,6 +14,7 @@ import javax.validation.ConstraintViolation;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.Vector;
 
 public class ChatWSController extends TextWebSocketHandler {
 
@@ -23,6 +25,22 @@ public class ChatWSController extends TextWebSocketHandler {
 
     private List<WebSocketSession> webSocketSessionList = new ArrayList<>();
 
+    private final List<MessageTO> chatMessage = new Vector<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        webSocketSessionList.add(session);
+
+        for (MessageTO previousMessages : chatMessage) {
+            String jsonMessage = mapper.writeValueAsString(previousMessages);
+            session.sendMessage(new TextMessage(jsonMessage));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        webSocketSessionList.remove(session);
+    }
 
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
@@ -31,6 +49,7 @@ public class ChatWSController extends TextWebSocketHandler {
             Set<ConstraintViolation<MessageTO>> violations = validatorFactoryBean.getValidator().validate(messageObj);
 
             if (violations.isEmpty()) {
+                chatMessage.add(messageObj);
                 for (WebSocketSession webSocketSession : webSocketSessionList) {
                     if (webSocketSession == session) continue;
                     if (webSocketSession.isOpen()) {
