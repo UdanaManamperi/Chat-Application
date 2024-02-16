b35b52e18c34d50891d1136d5a790a39a7acb026
udana <udanaoneplus@gmail.com>
2024-02-16 10:56:49 +0530

    Finalize backend

--- backend-ChatApp/src/main/java/lk/ijse/dep11/WebSocketConfig.java
+++ backend-ChatApp/src/main/java/lk/ijse/dep11/WebSocketConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
@@ -32,7 +33,7 @@ public class WebSocketConfig  implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWSController(), "api/v2/messages").setAllowedOrigins("*");
+        registry.addHandler(chatWSController(), "api/v2/messages").setAllowedOriginPatterns("*");
 
     }
 }
--- backend-ChatApp/src/main/java/lk/ijse/dep11/to/MessageTO.java
+++ backend-ChatApp/src/main/java/lk/ijse/dep11/to/MessageTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
 import java.io.Serializable;
 
 @Data
@@ -14,6 +15,14 @@ import java.io.Serializable;
 public class MessageTO implements Serializable {
     @NotBlank(message = "Chat message cannot be empty")
     String message;
+
+    @NotEmpty(message = "Email can't be empty")
     @Email(message = "Invalid email")
     String email;
+
+
+    @NotEmpty(message = "Name can't be empty")
+    private String name;
+    @NotEmpty(message = "Picture can't be empty")
+    private String picture;
 }
